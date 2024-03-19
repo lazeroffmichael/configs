99187dd8760cc342c381c2653539ac6ff467e29f
Michael Lazeroff <michaeldlazeroff@gmail.com>
2024-03-18 20:48:35 -0700

    parrot: add fonts and change apt

--- linux/parrot.sh
+++ linux/parrot.sh
@@ -1,5 +1,15 @@
+sudo apt update && sudo apt upgrade -y
+
+# Enable natural scrolling
 XORG_CONF=/usr/share/X11/xorg.conf.d/40-libinput.conf
-sudo apt-get update && sudo apt-get full-upgrade -y
 sudo sed -i '/MatchDevicePath "\/dev\/input\/event\*"/a \        Option "NaturalScrolling" "on"' $XORG_CONF
+
+# UTM Guest Tools
 sudo apt install spice-vdagent
+
+# Install UbuntuMono nerd font
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/UbuntuMono/Regular/UbuntuMonoNerdFontMono-Regular.ttf -P ~/Downloads
+mkdir -p ~/.local/share/fonts
+mv ~/Downloads/UbuntuMonoNerdFontMono-Regular.ttf ~/.local/share/fonts
+
 sudo systemctl --message="Restaring VM to apply updates" reboot
