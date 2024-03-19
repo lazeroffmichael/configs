3ff102b8b989d812a6f94c59fdcf2eaacffaa62e
Michael Lazeroff <michaeldlazeroff@gmail.com>
2024-03-18 20:17:18 -0700

    add reboot to linux/parrot.sh

--- linux/parrot.sh
+++ linux/parrot.sh
@@ -2,4 +2,4 @@ XORG_CONF=/usr/share/X11/xorg.conf.d/40-libinput.conf
 sudo apt-get update && sudo apt-get full-upgrade -y
 sudo sed -i '/MatchDevicePath "\/dev\/input\/event\*"/a \        Option "NaturalScrolling" "on"' $XORG_CONF
 sudo apt install spice-vdagent
-echo "RESTART VM TO GET ALL EFFECTS"
+sudo systemctl --message="Restaring VM to apply updates" reboot
